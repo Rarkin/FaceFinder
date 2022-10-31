818a5ede425936c152cdde019d5d27cd7063350a
Karl <karlgraves28@gmail.com>
2022-11-01 09:27:46 +1100

    initial set up and layout

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Tilt from 'react-parallax-tilt';
+
+const Logo = () => {
+    return(
+
+            <div className="ma7 mt1">
+                <div className="br2 shadow-2">👀</div>                            
+            </div>
+
+
+    );
+}
+
+export default Logo
