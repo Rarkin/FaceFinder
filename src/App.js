35f17281bb2729c15e0e72b2bd32861185fbfef4
Karl <karlgraves28@gmail.com>
2022-11-03 08:45:13 +1100

    added frontend components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-        {/* {<ImageLinkForm />
-        <FaceRecognition />}} */}
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
